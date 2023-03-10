d699d2ca6bb1dd550706975bc7bfbd282e03bf1b
Rasheed124 <rashdevatrealglobe.com>
2023-03-10 11:35:15 +0100

    starter template

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h2 className='font-bold text-center text-red-900 text-3xl'>Hello world</h2>
+      <h3>Am a web Developer in  lagos</h3>
     </div>
   );
 }
