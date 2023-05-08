a8475fee15257f3052824eaaeb46daafc7c55825
Rasheed124 <rashdevatrealglobe.com>
2023-05-08 18:08:21 +0100

    Starter Template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="font-bold text-center text-3xl text-orange-300">Welcome to my React Project Template</h1>
     </div>
   );
 }
