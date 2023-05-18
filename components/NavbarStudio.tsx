a2e3265e0c7c9f9ab2dc6115934bf601bdbd5291
Rasheed124 <rashdevatrealglobe.com>
2023-05-18 12:39:31 +0100

    Blog Updated

---
+++ components/NavbarStudio.tsx
@@ -0,0 +1,21 @@
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid"
+import Link from "next/link"
+
+
+const NavbarStudio = (props: any) => {
+    return (
+        // Render Default Studio Navbar
+        <div>
+
+            <div className="flex justify-start items-center p-6 ">
+                <Link href="/" className="text-[#F7AB0A] flex items-center uppercase">
+                    <ArrowUturnLeftIcon className="h-6 w-6 text-[#F7AB0A] mr-2" />
+                    Go To website
+                </Link>
+            </div>
+            <>{props.renderDefault(props)}</>
+        </div>
+    )
+}
+
+export default NavbarStudio
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,13 +2,16 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...index]]\page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
+import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from './sanity/env'
 import { schema } from './sanity/schema'
+import { myTheme } from './theme'
+import NavbarStudio from './components/NavbarStudio'
+import Logo from './components/Logo'
 
 export default defineConfig({
   basePath: '/studio',
@@ -18,6 +21,14 @@ export default defineConfig({
   dataset,
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
+  studio: {
+    components: {
+      logo: Logo,
+      navbar: NavbarStudio,
+    }
+  },
+  theme: myTheme,
+
   plugins: [
     deskTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
