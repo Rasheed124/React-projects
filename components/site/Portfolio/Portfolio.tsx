522ca313b03b880418628e0ae75167b76880843b
Rasheed124 <rashdevatrealglobe@gmail.com>
2023-06-21 10:03:24 -1100

    Portfolio Page Updated

--- components/site/Portfolio/Portfolio.tsx
+++ components/site/Portfolio/Portfolio.tsx
@@ -1,6 +1,7 @@
 
+"use client";
 
-import Link from "next/link";
+
 import ClientSideRoute from "../ClientSideRoute";
 import { motion } from "framer-motion";
 
@@ -16,13 +17,13 @@ const PortfolioProject = ({projects} : Props) => {
         <>
            {projects.map( project => (
 
-               <div className="bg-light-white py-20">
+               <div className="bg-light-white py-6">
 
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-3">
+                  <div className="grid grid-cols-1 gap-9 md:grid-cols-2 lg:grid-cols-3 p-3">
                         <motion.div
 
                        key={project._id}
-                            className={`min-h-screen bg-cover bg-no-repeat bg-center bg-deep-black bg-blend-overlay bg-opacity-10 `} style={{ backgroundImage: `url(${project.heading})` }} 
+                            className={`min-h-screen bg-cover bg-no-repeat bg-center bg-deep-black bg-blend-overlay bg-opacity-10 `} style={{ backgroundImage: `url(${project.projectImage})` }} 
                             initial="hidden"
                             whileInView="visible"
                             viewport={{ once: true, amount: 0.8 }}
