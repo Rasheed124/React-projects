ccc6c2d4537e48e9910b207a3509c56d7691cb87
Rasheed124 <rashdevatrealglobe@gmail.com>
2023-07-02 13:23:25 -1100

    Studio Navbar Updated

--- components/studio/Navbar.tsx
+++ components/studio/Navbar.tsx
@@ -4,16 +4,25 @@ import Link from "next/link"
 
 const Navbar = (props: any) => {
     return (
-        // Render Default Studio Navbar
+        // Render  Studio Navbar
         <div>
 
             <div className=" p-6 text-light-white">
 
-                <div className="flex justify-start items-start">
+                <div className="flex space-y-3 md:space-y-0 flex-col md:flex-row justify-between  items-center">
                    <Link href="/" className=" flex items-center uppercase">
-                      <ArrowUturnLeftIcon className="h-6 w-6  mr-2" />
+                      <ArrowUturnLeftIcon className="h-6 w-6  mr-2 mb-3 md:mb-0" />
                       Go To website
                   </Link>
+                  <div className="flex items-center space-x-2">
+                  <Link href="/api/preview" className="font-bold text-lg font-Antonio border px-5 py-2">
+                    Go Preview
+                  </Link>
+                  <Link href="/api/exit-preview"  className="font-bold text-lg font-Antonio  border px-5 py-2">
+                    Exit Preview
+                  </Link>
+                  </div>
+                 
                 </div>
            
 
