7b7c6aa0bd9c10bf462bb4a3db393957c8d5ac48
Rasheed124 <rashdevatrealglobe@gmail.com>
2024-06-14 16:03:39 +0100

    Authentication finished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,23 +3,21 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 // import { buttonVariants } from './ui/button'
 import { ArrowRight } from 'lucide-react'
 import { buttonVariants } from './ui/button'
-// import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 const Navbar = async () => {
 
-    const user =  undefined
-    const isAdmin = false
-//   const { getUser } = getKindeServerSession()
-//   const user = await getUser()
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
 
-//   const isAdmin = user?.email === process.env.ADMIN_EMAIL
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL
 
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
       <MaxWidthWrapper>
         <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
           <Link href='/' className='flex z-40 font-semibold'>
-            case<span className='text-green-600'>cobra</span>
+            raah<span className='text-green-600'>catstore</span>
           </Link>
 
           <div className='h-full flex items-center space-x-4'>
