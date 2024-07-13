2b5d8f4e2d061b4d7b25a50c660bf29716ce9df8
Rasheed124 <rashdevatrealglobe@gmail.com>
2024-07-13 03:09:15 +0100

    login & signup added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import LoginSignUp from "./pages/LoginSignUp";
+
+
+function App() {
+  return (
+    <>
+     <div>
+        <Navbar />
+
+        <LoginSignUp />
+     </div>
+    </>
+  );
+}
+
+export default App;
